0cf7190d48f90a8186e3f55f6e1a894ee7cfc73a
Yeju Lee (Julie) Motley <149555844+yjlmotley@users.noreply.github.com>
2024-08-15 14:45:01 +0000

    fixed syntax issue v2

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -8,20 +8,19 @@ export const Home = () => {
 	const { actions, store } = useContext(Context);
 
 	return (
-		<>
-			<div className="container pb-1">
-				<div className="d-flex justify-content-end">
-					<Link to="/addContact">
-						<button className="btn btn-success my-3">Add new contact</button>
-					</Link>
-				</div>
-				<div
-					id="contacts"
-					className="panel-collapse collapse show mb-5"
-					aria-expanded="true">
-					{store.contacts.length === 0 ? (
-							<li className="list-group-item no-tasks">-- You currently do not have any contacts, please add a contact --</li>
-						) : (
+		<div className="container pb-1">
+			<div className="d-flex justify-content-end">
+				<Link to="/addContact">
+					<button className="btn btn-success my-3">Add new contact</button>
+				</Link>
+			</div>
+			<div
+				id="contacts"
+				className="panel-collapse collapse show mb-5"
+				aria-expanded="true">
+				{store.contacts.length === 0 ? (
+					<li className="list-group-item no-tasks">-- You currently do not have any contacts, please add a contact --</li>
+				) : (
 					<ul className="list-group pull-down" id="contact-list">
 						{store.contacts.map((contact, index) => (
 							<ContactCard
@@ -31,8 +30,9 @@ export const Home = () => {
 							/>
 						))}
 					</ul>
-				</div>
+				)
+				}
 			</div>
-		</>
+		</div>
 	);
 };
